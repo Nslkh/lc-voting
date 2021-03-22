ce802e02c881b35129769c0dec1fdf15cdb733e5
Nslkh <olluzran@gmail.com>
2021-03-22 15:12:43 +0500

    Login & Register implemented

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,19 @@
         <header class="flex flex-col md:flex-row items-center justify-between px-8 py-4">
             <a href="#">Laracast Logo</a>
             <div class="flex items-center mt-2 md:mt-0">
+                @if (Route::has('login'))
+                    <div class=" px-6 py-4 ">
+                        @auth
+                            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                        @else
+                            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
                 <a href="#">
                     <img src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp" alt="avatar" class="w-10 h-10 rounded-full">
                 </a>
