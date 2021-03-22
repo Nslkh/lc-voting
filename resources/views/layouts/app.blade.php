517ed5f20b1b72351fde30b27eabaf93ccfc77ea
Nslkh <olluzran@gmail.com>
2021-03-22 18:25:07 +0500

    Design , Breeze installation, Setup Done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,8 +48,31 @@
                 </a>
             </div>
         </header>
-
         
+        <main class="container mx-auto max-w-custom flex" >
+            <div  class="w-70 mr-5">
+                Add Idea Form goes here 
+            </div>
+            <div class="w-175" >
+                <nav class="flex items-center justify-between text-xs">
+                   
+                    <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                        <li><a href="#" class="border-b-4 pb-3 border-blue">All Ideas (87)</a></li>
+                        <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering (6)</a></li>
+                        <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">In Progress (1)</a></li>
+                    </ul>
+                    
+                    <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                        <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Implemented (10)</a></li>
+                        <li><a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed (55)</a></li>
+                    </ul>
+                </nav>
+
+                <div class="mt-8">
+                    {{ $slot}}
+                </div>
+            </div>
+        </main>
       
     </body>
 </html>
